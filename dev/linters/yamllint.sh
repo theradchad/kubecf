d36e1c653d02f588d1e8d41890d9791e05118871
Mark Yen <mark.yen@suse.com>
2020-01-23 12:11:52 -0800

    fix: yamllint: skip template yaml files.

--- dev/linters/yamllint.sh
+++ dev/linters/yamllint.sh
@@ -4,11 +4,23 @@ set -o errexit -o nounset
 
 workspace=$(bazel info workspace)
 
+findcmd=(
+    find "${workspace}"
+    -type f
+    '('
+        -path "${workspace}/deploy/helm/kubecf/values.*"
+        -or
+            '(' -not -path "${workspace}/deploy/helm/kubecf/*" ')'
+    ')'
+    '('
+        '(' -name '*.yaml' -not -name '*.tmpl.yaml' ')'
+        -or
+        '(' -name '*.yml' -not -name '*.tmpl.yaml' ')'
+    ')'
+)
+
 # shellcheck disable=SC2046
 # We want word splitting with find.
 bazel run //dev/linters/yamllint -- \
       -d "{extends: relaxed, rules: {line-length: {max: 120}}}" \
-      --strict $(find "${workspace}" -type f \
-                      -path "${workspace}/deploy/helm/kubecf/values.*" \
-                      -or -not -path "${workspace}/deploy/helm/kubecf/*" \
-                      -name '*.yaml' -or -name '*.yml')
+      --strict $("${findcmd[@]}")
